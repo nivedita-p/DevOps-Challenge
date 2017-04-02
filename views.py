c043d18e14a819e0260d4f3768d4d752d796d139
nivedita-p <niveditapaul89@gmail.com>
2017-04-02 13:57:29 +0200

    Add files via upload

---
+++ views.py
@@ -0,0 +1,28 @@
+# app/views.py
+
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from bs4 import BeautifulSoup
+import requests
+import re
+# Create your views here.
+
+class HomePageView(TemplateView):
+   
+
+    def scanPage(request):
+        url = requests.get("https://boards.greenhouse.io/definiens#.WNAYbFUrLX5")
+        content = url.content
+       # print (content)
+        soup = BeautifulSoup(content,'html.parser')
+        links = soup.findAll('a')
+       # link = soup.findAll("a")
+        for link in links:
+            print (link)
+            if link.find(text=re.compile("DevOps")):
+                thelink =  link 
+                break
+
+        print (thelink)
+        return HttpResponse(thelink)
